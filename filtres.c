5ec7c1d7f9a184f9ff05275acb4cd199dc0bc1c0
Тоня <140175414+tonyyya@users.noreply.github.com>
2026-01-14 18:01:03 +0300

    Update filtres.c
    + горизонтальное размытие

--- filtres.c
+++ filtres.c
@@ -21,6 +21,35 @@ void apply_grayscale(image* img) {
 }
 
 
+void brush(const image* in, image* out) { \\ горизонтальное размытие
+    if (!in  !out) return;
+    if (in->width != out->width  in->height != out->height) return;
+
+    int w = in->width;
+    int h = in->height;
+    const int radius = 3; // длина мазка 7 пикселей
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            long sum_r = 0, sum_g = 0, sum_b = 0;
+            int count = 0;
+
+            // горизонтальный мазок
+            for (int dx = -radius; dx <= radius; dx++) {
+                pixel p = get_pixel_clamped(in, x + dx, y);
+                sum_r += p.r;
+                sum_g += p.g;
+                sum_b += p.b;
+                count++;
+            }
+
+            out->data[y][x].r = (uint8_t)(sum_r / count);
+            out->data[y][x].g = (uint8_t)(sum_g / count);
+            out->data[y][x].b = (uint8_t)(sum_b / count);
+        }
+    }
+}
+
 //фильтр негативный (-neg) (я осуждаю негатив)
 void apply_negative(image* img) {
 
@@ -66,5 +95,6 @@ void apply_crop(image* img, int new_widht, int new_height) {
     img -> data = new_data;
     img -> wight = w;
     img -> height = h;
+
 
     return 0; }
